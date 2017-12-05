f4740d2557e36af3e3d865866cd1d605d54046d8
Lex Martinez <lex0316@gmail.com>
2017-12-04 21:42:53 -0500

    first search

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const puppeteer = require('puppeteer')
+
+async function run() {
+
+ const browser = await puppeteer.launch({headless: false})
+ const page = await browser.newPage()
+
+ await page.goto('https://www.tucarro.com.co/')
+ await page.waitForSelector('.nav-search-submit')
+ await page.click('button[type=submit]');
+
+  browser.close()
+
+}
+
+run()
+
